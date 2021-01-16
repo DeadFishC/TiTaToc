325d95c2450fadaa185e388ee395b11dde7477ee
NikitaK <cssmast@gmail.com>
2021-01-16 17:34:44 +0300

    Design and animations. Game works. TODO: Play again, show winning line

--- js/main.js
+++ js/main.js
@@ -1,21 +1,24 @@
+let activePlayer, player1, player2;
+
 const gameBoard = (() => {
-    let _board = [-1,-1,-1,-1,-1,-1,-1,-1,-1];
+    let _board = ['E','E','E','E','E','E','E','E','E'];
     let _count = 0;
 
-    const play = (place, sign) => { _board[place] = sign; _count++;};
-    const clear = () => {_board = [-1,-1,-1,-1,-1,-1,-1,-1,-1]; _count = 0};
+    const play = (place, sign) => { _board[place] = sign; _count++; console.log(_board);};
+    const clear = () => {_board = ['E','E','E','E','E','E','E','E','E']; _count = 0};
     const checkWin = () => {
+        console.log('_count: '+ _count);
         for (let i = 0; i < 9; i += 3) {
-            if (_board[i] == -1 && _board[i] ==_board[i+1] && _board[i+1] == _board[i+2])
+            if (_board[i] != 'E' && _board[i] ==_board[i+1] && _board[i+1] == _board[i+2])
                 return _board[i];
         }
         for (let i = 0; i < 3; i++) {
-            if (_board[i] != -1 && _board[i] == _board[i+3] && _board[i+3] == _board[i+6])
+            if (_board[i] != 'E' && _board[i] == _board[i+3] && _board[i+3] == _board[i+6])
                 return _board[i];
         }
-        if (_board[0] != -1 && _board[0] == _board[4] && _board[4] == _board[8])
+        if (_board[0] != 'E' && _board[0] == _board[4] && _board[4] == _board[8])
             return _board[0];
-        if (_board[2] != -1 && _board[2] == _board[4] && _board[4] == _board[6])
+        if (_board[2] != 'E' && _board[2] == _board[4] && _board[4] == _board[6])
             return _board[2];
         if (_count == 9)
             return 'Tie';
@@ -28,24 +31,51 @@ const gameBoard = (() => {
     };  
 })();
 
-let deleteMe = 0;
 $(document).on('click', '.tile', function () {
     if (!this.hasChildNodes()) {
-        if (deleteMe == 0) {
-            $('#' + this.id).append('<img src="img/circle.svg" class="circle" alt="Circle" onload="SVGInject(this)">');
-            deleteMe++;
+        $('#' + this.id).append(`<img src="${activePlayer.sign == 'X' ? 'img/cross.svg' : 'img/circle.svg'}" class="circle" alt="Circle" onload="SVGInject(this)">`);
+        gameBoard.play(this.id, activePlayer.sign);
+        let result = gameBoard.checkWin();
+        console.log("result: " + result);
+        if (result == 'Continue') {
+            activePlayer = activePlayer == player2 ? player1 : player2;
+            return;
+        }
+        if (result == 'Tie') {
+            $('#message').text("It's a tie!");
+            $('#message').css("visibility", "visible");
+            return;
+        }
+        if (result == 'X') {
+            $('#message').text(`${player1.name} wins!`);
+            $('#message').css("visibility", "visible");
+            return;
         }
         else {
-            $('#' + this.id).append('<img src="img/cross.svg" class="circle" alt="Circle" onload="SVGInject(this)">');
-            deleteMe--;
+            $('#message').text(`${player2.name} wins!`);
+            $('#message').css("visibility", "visible");
         }
+
     }
 });
 
+const start = () => {
+    let validation = () => {
+        
+    }
 
+    player1 = playerFactory('X', $('#player1_name').val());
+    player2 = playerFactory('O', $('#player2_name').val());
+    let roll = Math.floor(Math.random() * Math.floor(2));
+    if (roll == 0)
+        activePlayer = player1;
+    else
+        activePlayer = player2;
+    $('.playerField').addClass('fadeout');
+    $('.grid_container').addClass('showGridLines');
+    $('.wrapper').addClass('gridPulse');
+}
 
 const playerFactory = (sign, name) => {
     return {sign, name};
 };
-
-player = playerFactory('x', 'Kek');
